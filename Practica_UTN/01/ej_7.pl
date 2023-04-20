7bb748ecd7d1566b5a71c7711c8468c9078237fb
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-04-20 11:32:00 -0300

    uploading ex's

---
+++ Practica_UTN/01/ej_7.pl
@@ -0,0 +1,21 @@
+/*
+    Escribir un programa que simule una calculadora para las operaciones matemáticas básicas 
+    (suma, resta, multiplicación y división) entre dos valores numéricos, informando el resultado.
+*/
+
+%hechos
+suma(1,1,2).
+suma(1,2,3).
+suma(1,3,4).
+
+resta(1,1,0).
+resta(1,2,-1).
+resta(1,3,-2).
+
+multiplicacion(1,1,1).
+multiplicacion(1,2,2).
+multiplicacion(1,3,3).
+
+division(1,1,1).
+division(1,2,0.5).
+division(1,3,0.3333333333333333).

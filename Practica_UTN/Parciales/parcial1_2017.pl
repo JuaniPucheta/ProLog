452f539c29675aa3bb95dc9255c73cd7cc941766
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-06-24 16:32:35 -0300

    parcial1_2017/9

---
+++ Practica_UTN/Parciales/parcial1_2017.pl
@@ -0,0 +1,55 @@
+/* Ejercicio 1
+    En una base de datos de una empresa de venta de robots domesticos, se tiene la 
+    siguiente estructura de hechos:
+
+    compro_robot(persona, codigo_robot, nombreModelo).
+    modelo_marca(nombreModelo, marca).    
+
+    Hacer un programa en ProLog que permita ingresar una persona y una marca, e informe
+    de manera intelegible si la persona compro o no compro algun modelo de esa marca.
+*/
+
+compro_robot(julian, a001, roomba).
+compro_robot(franco, q900, zenbo).
+compro_robot(eduardo, h010, aspiradoraLG).
+
+modelo_marca(roomba, iRobot).
+modelo_marca(zenbo, asus).
+modelo_marca(aspiradoraLG, lg).
+
+inicio:- write("Ingrese una persona: "), read(Persona),
+    write("Ingrese una marca: "), read(Marca),
+    compro_robot(Persona, _, Modelo),
+    modelo_marca(Modelo, Marca),
+    write("La persona "), write(Persona), 
+        write(" compro el modelo "), write(Modelo), 
+        write(" de la marca "), write(Marca).
+inicio:- write("La persona no compro ningun modelo de esa marca.").
+
+/* Ejercicio 2
+    Siguiendo con el caso de la empresa de venta de robots domesticos, hacer un 
+    programa en ProLog que permita ingresar una lista con el precio de cada uno
+    de sus productos (se ingresa solo el precio) e informe:
+    1. El precio minimo.
+    2. El precio maximo.
+
+    Por ejemplo: la lista [2000, 1500, 1500, 3000] corresponde al precio de 4 robots 
+    domesticos de la empresa. El precio minimo en este caso es de 1500 y el maximo 3000
+*/
+
+inicio2:- write("Ingrese una lista de precios: "), leer([H|T]),
+    minimo(T, H, Minimo), maximo(T, H, Maximo),
+    write("El precio minimo es: "), write(Minimo), nl,
+    write("El precio maximo es: "), write(Maximo).
+
+leer([H|T]):- read(H), H\=[], leer(T).
+leer([]).
+
+minimo([], Min, Min).
+minimo([H|T], Min0, Min):- H < Min0, minimo(T, H, Min).
+minimo([_|T], Min0, Min):- minimo(T, Min0, Min).
+
+maximo([], Max, Max).
+maximo([H|T], Max0, Max):- H > Max0, maximo(T, H, Max).
+maximo([_|T], Max0, Max):- maximo(T, Max0, Max).
+
---
+++ Practica_UTN/Parciales/parcial1_2019.pl
@@ -0,0 +1,58 @@
+/* Ejercicio 1
+    En una base de datos de una empresa de venta de autos autónomos, 
+    se tiene la siguiente estructura de hechos:
+    - compro_auto_autonomo(persona, anioCompra, nombreModelo).
+    - modelo_marca(nombreModelo, marca).
+
+    Como por ejemplo:
+    compro_auto_autonomo(pablo, 2019, 'Volvo XC90').
+    compro_auto_autonomo(alejandro, 2017, 'Chrysler pacifica').
+
+    modelo_marca('Volvo XC90', uber).
+    modelo_marca('Chrysler pacifica', waymo).
+
+    Hacer un programa en ProLog que permita ingresar una persona, un anio y una marca,
+    e informe si la persona NO compro algun modelo de esa marca ese anio con un mensaje
+    con la siguiente estructura:
+    "La persona...no compro un auto de marca...en el anio..."
+*/
+
+compro_auto_autonomo(pablo, 2019, 'Volvo XC90').
+compro_auto_autonomo(alejandro, 2017, 'Chrysler pacifica').
+
+modelo_marca('Volvo XC90', uber).
+modelo_marca('Chrysler pacifica', waymo).
+
+inicio :- write("Ingrese una persona: "), read(Persona),
+    write("Ingrese un anio: "), read(AnioCompra),
+    write("Ingrese el nombre del modelo: "), read(NombreModelo),
+    not(compro_auto_autonomo(Persona, AnioCompra, NombreModelo)),
+    modelo_marca(NombreModelo, Marca),
+    write("La persona "), write(Persona), write(" no compro un auto de marca "), write(Marca), write(" en el anio "), write(AnioCompra), write("."), nl.
+
+/* Ejercicio 2
+    Siguiendo el caso de la empresa de venta de autos autónomos hacer un programa   
+    en ProLog que permita ingresar una lista con el precio de cada uno de ellos 
+    (se ingresa solo el precio) e informe:
+    1. La suma del primer elemento de la lista, mas el segundo, mas el ultimo.
+    2. El penultimo elemento de la lista
+*/
+
+inicio2:- write("Ingrese una lista de precios: "), leer(Lista),
+    dosPrimeros(Lista, [H1, H2]),
+    ultimo(Lista, Ultimo),
+    Suma is H1 + H2 + Ultimo,
+    write("La suma del primer elemento de la lista, mas el segundo, mas el ultimo es: "), write(Suma), nl,
+    penultimo(Lista, Penultimo),
+    write("El penultimo elemento de la lista es: "), write(Penultimo), nl.
+
+leer([H|T]) :- read(H), H \= [], leer(T).
+leer([]).
+
+dosPrimeros([H1, H2 | _], [H1, H2]).
+
+ultimo([H|[]], H).
+ultimo([_|T], Ultimo) :- ultimo(T, Ultimo).
+
+penultimo([X,_], X).
+penultimo([_|T], X) :- penultimo(T, X).

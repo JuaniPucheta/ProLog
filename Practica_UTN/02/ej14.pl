9bac8c409994f0695a2b315d0512f6471d2c2222
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2024-01-19 13:44:11 -0300

    resolution v2

--- Practica_UTN/02/ej14.pl
+++ Practica_UTN/02/ej14.pl
@@ -13,3 +13,11 @@ pertenece(X,[_|T]):- pertenece(X,T).
 repite(_,[]):- write("No hay elementos que se repitan").
 repite(X,T):- pertenece(X,T), write("El primer elemento que se repite es: "), write(X).
 repite(_,[X|T]):- repite(X,T).
+
+%* Otra forma
+    % pertenece(X, [X|_]).
+    % pertenece(X, [_|T]):- pertenece(X, T).
+
+    % primer_repetido([H|T], H):- pertenece(T, H), write("El primer elemento repetido es: "), writeln(H).
+    % primer_repetido([_|T]):- primer_repetido(T).
+    % primer_repetido([]):- write("No hay elementos repetidos").
